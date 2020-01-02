e6a38e7c2fdd2c08a8a2c1b7d762cfc46feb7fe5
gantushig21 <gantushig@infinite.mn>
2020-01-02 16:43:39 +0800

    Next problem is usaco section 3.2 kimbits

--- usaco/section_3.2/kimbits.java
+++ usaco/section_3.2/kimbits.java
@@ -2,36 +2,19 @@
 /*
 ID: gantush6
 LANG: JAVA
-TASK: fact4
+TASK: kimbits
 */
 
 import java.io.*;
 import java.util.*;
 
-public class fact4 {
+public class kimbits {
     public static void main(String[] args) throws IOException {
-        FastScanner input = new FastScanner(new FileReader("fact4.in"));
-        PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("fact4.out")));
+        FastScanner input = new FastScanner(new FileReader("kimbits.in"));
+        PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("kimbits.out")));
 
-        int n = input.nextInt();
-        int digit = 1, s2 = 0, s5 = 0;
-        for (int i = 2; i <= n; i++) {
-            int temp = i;
-            while (temp % 2 == 0) {
-                s2++;
-                temp /= 2;
-            }
-            while (temp % 5 == 0) {
-                s5++;
-                temp /= 5;
-            }
+        int N = input.nextInt(), L = input.nextInt(), I = input.nextInt();
 
-            digit = (digit * temp) % 10;
-        }
-        for (int i = 0; i < s2 - s5; i++)
-            digit = (digit * 2) % 10;
-
-        output.println(digit);
         output.close();
     }
 
